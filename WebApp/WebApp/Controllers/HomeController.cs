37513b66a0e9575ec33819d065d3074934dc74f6
Yuri <Yuri@DESKTOP-O3TAU8D>
2019-08-20 21:57:13 -0300

    Novo

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -12,7 +12,6 @@ namespace WebApp.Controllers
         {
             return View();
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
